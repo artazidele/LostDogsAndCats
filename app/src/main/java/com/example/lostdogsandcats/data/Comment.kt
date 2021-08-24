9a95feb09dcb9b41066f382b6740c9c9760fa3c4
artazidele <77989249+artazidele@users.noreply.github.com>
2021-08-24 06:52:29 +0300

    Changed data classes

--- app/src/main/java/com/example/lostdogsandcats/data/Comment.kt
+++ app/src/main/java/com/example/lostdogsandcats/data/Comment.kt
@@ -1,8 +1,8 @@
 package com.example.lostdogsandcats.data
 
 data class Comment(
-    var petId: String,
-    var userId: String,
-    var commentText: String,
-    var date: String
+    var petId: String = "",
+    var userId: String = "",
+    var commentText: String = "",
+    var date: String = ""
 )
--- app/src/main/java/com/example/lostdogsandcats/data/LostPet.kt
+++ app/src/main/java/com/example/lostdogsandcats/data/LostPet.kt
@@ -12,13 +12,13 @@ package com.example.lostdogsandcats.data
  * @property userId
  */
 data class LostPet(
-    val petId: String,
-    val name: String,
-    val isDog: Boolean,
-    var description: String,
-    var photo: String,
-    var date: String,
-    var place: String,
-    var number: String,
-    var userId: String
+    val petId: String = "",
+    val name: String = "",
+    val isDog: Boolean = false,
+    var description: String = "",
+    var photo: String = "",
+    var date: String = "",
+    var place: String = "",
+    var number: String = "",
+    var userId: String = ""
 )
